40813c032a64a5b0658d6f196d52d6ca5353683a
Yury Ostroukh <0str0uchura@gmail.com>
2018-10-01 15:17:41 +0300

    Prevent to edit CT in History tab

--- src/ggrc-client/js/components/workflow/cycle-task-group-object-task/cycle-task-group-object-task.js
+++ src/ggrc-client/js/components/workflow/cycle-task-group-object-task/cycle-task-group-object-task.js
@@ -11,9 +11,43 @@ import '../../comment/comment-add-form';
 import '../../comment/mapped-comments';
 import {updateStatus} from '../../../plugins/utils/workflow-utils';
 import {getPageType} from '../../../plugins/utils/current-page-utils';
+import Permission from '../../../permission';
+import Stub from '../../../models/stub';
+import {getModelInstance} from '../../../plugins/utils/models-utils';
 
 let viewModel = can.Map.extend({
   define: {
+    isInHistory: {
+      get(lastValue, setValue) {
+        const cycleStub = new Stub(this.attr('instance.cycle'));
+        let promise = Promise.resolve(false);
+
+        /**
+         * We check permissions here because current user can have
+         * EDIT rights for current cycle task, but doesn't have
+         * READ rights for appropriate cycle and workflow (for example,
+         * the user is a Global Creator and assigned to Cycle Task) - it
+         * means that he is able to edit mentioned cycle task, but cannot
+         * get cycle, in which it's included.
+         */
+        if (Permission.is_allowed_for('update', cycleStub)) {
+          promise = getModelInstance(
+            cycleStub.attr('id'),
+            cycleStub.attr('type'),
+            'is_current'
+          ).then((loadedCycle) => !loadedCycle.attr('is_current'));
+        }
+
+        promise.then(setValue);
+      },
+    },
+    isEditDenied: {
+      get() {
+        return !Permission
+          .is_allowed_for('update', this.attr('instance')) ||
+          this.attr('isInHistory');
+      },
+    },
     showWorkflowLink: {
       get() {
         return getPageType() !== 'Workflow';
